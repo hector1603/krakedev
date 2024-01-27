4207d58026adf4880777d05fce295bd6f359b01b
Hector Ajumado <hector.ajumado@gmail.com>
2024-01-27 07:36:19 -0400

    Cambiar funcionalidad al boton Presioname

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,7 @@
 saludar = function() {
+    let cmpImgSaludo;
+    cmpImgSaludo = document.getElementById("img-ganar");
+    cmpImgSaludo.src = "./img/ganar.gif";
+
     console.log("Hola desde el metaverso!!!");
 }
