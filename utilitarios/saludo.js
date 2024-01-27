90866e5416a64e933d18f5a3bf37165c3f0e471a
Hector Ajumado <hector.ajumado@gmail.com>
2024-01-27 09:48:45 -0400

    Implementar funciones recuperarInt y recuperarFlotante ok

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -6,9 +6,23 @@ recuperarTexto = function(idComponente) {
     return valorComponente;
 }
 
+recuperarInt = function(idComponente) {
+    let valor = recuperarTexto(idComponente);
+    let valorEntero = parseInt(valor);
+    return valorEntero;
+}
+
+recuperarFlotante = function(idComponente) {
+    let valor = recuperarTexto(idComponente);
+    let valorFlotante = parseFloat(valor);
+    return valorFlotante;
+}
+
 saludar = function() {
     let nombre = recuperarTexto("txtNombre");
     let apellido = recuperarTexto("txtApellido");
+    let edad = recuperarInt("txtEdad");
+    let estatura = recuperarFlotante("txtEstatura");
 
     alert("Hola " + nombre + " " + apellido);
 }
