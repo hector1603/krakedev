9d236da6585e328961a4cbd6d2f2727a3d78dade
Hector Ajumado <hector.ajumado@gmail.com>
2024-02-15 10:55:56 -0400

    Paso 1 - Guardar palabra validad ok

--- juego_ahorcado v1/ahorcado.js
+++ juego_ahorcado v1/ahorcado.js
@@ -1,4 +1,6 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta;
+
 esMayuscula = function(caracter) {
     let ascii = caracter.charCodeAt();
     if(ascii > 64 && ascii < 91) {
@@ -8,4 +10,29 @@ esMayuscula = function(caracter) {
     }
 }
 
-console.log(esMayuscula("s"));
+guardarPalabra = function() {
+    let palabra = recuperarTexto("txtSecreta");
+    let palabraMayuscula = false;
+
+    if(palabra.length == 5) {
+        for(let i = 0; i < palabra.length; i++) {
+            let letra = palabra.charAt(i);
+            if(esMayuscula(letra)) {
+                palabraMayuscula = true;
+            } else {
+                palabraMayuscula = false;
+                break;
+            }
+        }
+        
+        if(palabraMayuscula == true) {
+            palabraSecreta = palabra;
+            console.log(palabraSecreta);
+        } else {
+            alert("Ingresa una palabra de 5 letras mayusculas.");
+        }
+    } else {
+        alert("Error"); 
+    }
+
+}
