22494df5abe254569c9585581a5eae8847a878e8
Hector Ajumado <hector.ajumado@gmail.com>
2024-02-20 14:25:17 -0400

    Mostrar tabla empleados al terminar de cargar la primera vista

--- roles/roles.js
+++ roles/roles.js
@@ -1,10 +1,26 @@
 let empleados = [
-    {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"1714616123", nombre:"John", apellido:"Cena", sueldo:500.0},
+    {cedula:"0914632123", nombre:"Luisa", apellido:"Gonzalez", sueldo:900.0},
+    {cedula:"5231456985", nombre:"Mario", apellido:"Cruz", sueldo:560.0}
 ]
+
+mostrarEmpleados = function() {
+    let tablaEmpleados = document.getElementById("tablaEmpleados");
+    let crearTabla = "<table><tr><th>CEDULA</th><th>NOMBRE</th><th>APELLIDO</th><th>SUELDO</th></tr>";
+    for(let i = 0; i < empleados.length; i++) {
+        let empleado = empleados[i];
+        crearTabla += "<tr><td>" + empleado.cedula + "</td>"
+                    +"<td>" + empleado.nombre + "</td>"
+                    +"<td>" + empleado.apellido + "</td>"
+                    +"<td>" + empleado.sueldo + "</td></tr>";
+    }
+    crearTabla += "</table>";
+    tablaEmpleados.innerHTML = crearTabla;
+}
 
 mostrarOpcionEmpleado = function() {
     mostrarComponente("divEmpleado");
+    mostrarEmpleados();
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
 }
